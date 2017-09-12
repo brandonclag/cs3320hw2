97b81f4830b7280ab263aff5f347e39f9dfb62b5
brcla <brcla@10.216.214.239>
2017-09-12 10:27:34 -0500

    Finished product for hw2

--- src/edu/txstate/cs3320/claggett/Bio2.java
+++ src/edu/txstate/cs3320/claggett/Bio2.java
@@ -19,7 +19,11 @@ public class Bio2 {
 	private static BufferedWriter  writer = null;
 
 	private static final String TITLE = "My Life";
-
+	/**
+	 * main method used to kickstart the program. Starts the reading of paragraphs
+	 * initializes the output file, then writes to the output file
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
         readParagraphs(INPUT_FILE);
@@ -28,7 +32,10 @@ public class Bio2 {
 
 	}
 	
-	
+	/**
+	 * Takes the param of html code and writes it to the bio.html file using buffered writer
+	 * @param html
+	 */
 	private static void writeHTML(String html)
 	{
 		try {
@@ -40,6 +47,10 @@ public class Bio2 {
 
 	}
 	
+	/**
+	 * Takes the param of a string of the title. Inserts the correct html headings and writes the lines using writeHTML
+	 * @param title
+	 */
 	private static void writeHTMLOpening (String title) 
 	{
 		writeHTML(HTMLTags.HTML_HEADER_START);
@@ -48,13 +59,20 @@ public class Bio2 {
 		writeHTML(HTMLTags.BODY_START);
 	}
 	
-	
+	/**
+	 * Takes no param. Simply uses writeHTML to write the closing statement to the html file
+	 */
 	private static void writeHTMLClosing () 
 	{
 		writeHTML(HTMLTags.BODY_END);
 		writeHTML(HTMLTags.HTML_END);
 	}
-	
+	/**
+	 * Takes the name of the input file path and reads the info in the file. If an exception occurs, readParagraphs returns 
+	 * false 
+	 * @param inputFileName
+	 * @return successfulRead
+	 */
 	@SuppressWarnings("resource")
 	private static boolean readParagraphs(String inputFileName)
 	{
@@ -103,31 +121,56 @@ public class Bio2 {
 
 	}
 	
-	
+	/**
+	 * Uses a bufferedWriter to write each paragraph with the proper html tags
+	 */
 	private static void writeParagraphs () {
 		for (int i=0; i<headings.size(); i++) {
-		      writeHTML(HTMLTags.H1_START+ headings.get(i)+ HTMLTags.H1_END);
+		      writeHTML(HTMLTags.H1_START+ formatText(headings.get(i))+ HTMLTags.H1_END);
 		      writeHTML(HTMLTags.PARAGRAPH_START);
 		      writeHTML(paragraphs.get(i));
 		      writeHTML(HTMLTags.PARAGRAPH_END);
 		}
 	}
 	
-	
+	/**
+	 * Kicks off writing to the outputfile. Writes the HTMLopening, writes the paragraphs, then writes the closing
+	 * @param title
+	 */
 	private static void writeBio (String title) {
 		writeHTMLOpening (title);
 		writeParagraphs();
 		writeHTMLClosing();
 	}
 	
-	
+	/**
+	 * Initializes the bufferedWriter to create an output file named the param fileName
+	 * @param fileName
+	 */
 	private static void initialize (String fileName) {
 		try {
 			writer = new BufferedWriter (new FileWriter(fileName));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-           }
+    }
+
+	/**
+	 * strips the "[h1]" portion of each header
+	 * @param text
+	 * @return
+	 */
+	private static String formatText (String text) {
+		final String SYMBOL = "[h1]";
+		final int      SYMBOL_LENGTH = SYMBOL.length();
+		final  int NOT_FOUND = -1;
+		StringBuilder stringBuilder = new StringBuilder(text);
+		int indexOf = stringBuilder.indexOf(SYMBOL);
+		if (indexOf != NOT_FOUND) {
+			stringBuilder.replace(indexOf, indexOf+SYMBOL_LENGTH, "");
+		}
+		return stringBuilder.toString();
+	}
 
 
 
